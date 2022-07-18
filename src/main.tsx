9ccd18af01326cd7d8e954b5d255a04949333c76
Tachibana Shin <tachibshin@duck.com>
2022-07-18 16:34:37 +0000

    dark mode

--- src/main.tsx
+++ src/main.tsx
@@ -6,6 +6,7 @@ import "virtual:windi-devtools"
 
 import { App } from "./App"
 import "./main.scss"
+import { darkTheme } from "./darkTheme"
 import { store } from "./store"
 
 // eslint-disable-next-line import/order
@@ -13,11 +14,16 @@ import { BrowserRouter } from "react-router-dom"
 // eslint-disable-next-line import/order
 import { StrictMode } from "react"
 
+// eslint-disable-next-line import/order
+import ThemeProvider from "@mui/material/styles/ThemeProvider"
+
 ReactDOM.createRoot(document.getElementById("app") as HTMLElement).render(
   <StrictMode>
     <Provider store={store}>
       <BrowserRouter>
-        <App />
+        <ThemeProvider theme={darkTheme}>
+          <App />
+        </ThemeProvider>
       </BrowserRouter>
     </Provider>
   </StrictMode>
