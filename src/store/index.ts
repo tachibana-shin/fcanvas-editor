29100e6ad9c515ea4182170645386e19602760ae
Tachibana Shin <45375496+tachibana-shin@users.noreply.github.com>
2022-07-19 09:43:35 +0000

    install store

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,9 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit"
 
 import { editorStore } from "./editor"
+import { userStore } from "./user"
 
 export const store = configureStore({
   reducer: {
-    editor: editorStore.reducer
+    editor: editorStore.reducer,
+    user: userStore.reducer
   }
 })
