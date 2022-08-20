42ec465ba51854f7d44a4d52cfb3cebac50f1486
Tachibana Shin <45375496+tachibana-shin@users.noreply.github.com>
2022-08-20 04:07:37 +0000

    setup `console preview`

---
+++ src/components/sketch/injects/transport-console.ts
@@ -0,0 +1,22 @@
+import { Encode } from "vue-console-feed"
+
+console.log("transport-console actived!")
+for (const name in console) {
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  const fn = (console as unknown as any)[name]
+  // eslint-disable-next-line functional/functional-parameters, @typescript-eslint/no-explicit-any
+  ;(console as unknown as any)[name] = function (...messages: any[]) {
+    parent.postMessage({
+      type: `console.${name}`,
+      args: messages.map((item) => Encode(item, true, true))
+    })
+    return fn.call(this, ...messages)
+  }
+}
+
+addEventListener("error", (event) => {
+  parent.postMessage({
+    type: "error",
+    args: Encode(event.error, true, true)
+  })
+})
